9429523c49ca1e8686404e34458cd017bc3dd50b
Breno Vasconcelos <brenoman@gmail.com>
2015-06-22 15:15:03 -0300

    Added admin namespaced users controller

---
+++ spec/support/seed_helpers.rb
@@ -0,0 +1,10 @@
+module SeedHelpers
+  def create_user!(attributes={})
+    user = Factory(:user, attributes)
+    user.confirm!
+    user
+  end
+end
+Rspec.configure do |config|
+  config.include SeedHelpers
+end
